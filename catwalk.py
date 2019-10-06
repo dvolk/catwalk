af2e61d5aa3e873377f82c20149cd6a22bb4afe9
Denis Volk <denis.volk@gmail.com>
2019-10-06 23:06:56 +0100

    add alternative py sample impl

--- catwalk.py
+++ catwalk.py
@@ -2,11 +2,55 @@ import sys
 import glob
 import argh
 import profile
+from BitVector import BitVector
+from intset import IntSet
 
-def make_acgtn_set():
-    return { 'A': set(), 'C': set(), 'G': set(), 'T': set(), '-': set(), 'N': set() }
+def make_acgtn_set_bv(l):
+    return { 'A': BitVector(size=l),
+             'C': BitVector(size=l),
+             'G': BitVector(size=l),
+             'T': BitVector(size=l),
+             'N': BitVector(size=l),
+             '-': BitVector(size=l),
+    }
 
-class Sample:
+def make_acgtn_set(l):
+    return { 'A': set(),
+             'C': set(),
+             'G': set(),
+             'T': set(),
+             'N': set(),
+             '-': set(),
+    }
+
+class BVSample:
+    def __init__(self):
+        self.name = None
+        self.sequence = None
+        self.diff_sets = None
+        self.sequence_len = None
+
+    def from_fasta(self, contents):
+        xs = [x.strip() for x in contents.split('\n')]
+        self.name = xs[0][1:]
+        self.sequence = ''.join(xs[1:])
+        return self
+
+    def reference_compress(self, reference_sample):
+        self.diff_sets = make_acgtn_set_bv(len(reference_sample.sequence))
+        for i, (sample_base, reference_base) in enumerate(zip(self.sequence, reference_sample.sequence)):
+            #if sample_base != reference_base and sample_base not in ['N', '-'] and reference_base not in ['N','-']:
+            self.diff_sets[sample_base][i] = 1
+        self.sequence_len = len(self.sequence)
+        self.sequence = None
+
+    def diff_sample(self, sample2):
+        diff = BitVector(size = self.sequence_len)
+        for k in self.diff_sets.keys():
+            diff = diff | (self.diff_sets[k] ^ sample2.diff_sets[k])
+        return diff
+
+class FNSample:
     def __init__(self):
         self.name = None
         self.sequence = None
@@ -19,11 +63,17 @@ class Sample:
         return self
 
     def reference_compress(self, reference_sample):
-        self.diff_sets = make_acgtn_set()
+        self.diff_sets = make_acgtn_set(len(reference_sample.sequence))
         for i, (sample_base, reference_base) in enumerate(zip(self.sequence, reference_sample.sequence)):
             if sample_base != reference_base and sample_base not in ['N', '-'] and reference_base not in ['N','-']:
                 self.diff_sets[sample_base].add(i)
         self.sequence = None
+
+    def diff_sample(self, sample2):
+        diff = IntS
+        for k in list(self.diff_sets.keys()):
+            diff = diff | (self.diff_sets[k] ^ sample2.diff_sets[k])
+        return diff
 
 class CatWalk:
     def __init__(self, instance_name, instance_reference):
@@ -40,28 +90,25 @@ class CatWalk:
         for sample in samples:
             self.add_sample(sample)
 
-    def diff_samples(self, sample1_name, sample2_name):
+    def diff_sample(self, sample1_name, sample2_name):
         sample1 = self.samples[sample1_name]
         sample2 = self.samples[sample2_name]
-        diff = set()
-        for k in list(sample1.diff_sets.keys()):
-            diff = diff.union(sample1.diff_sets[k].symmetric_difference(sample2.diff_sets[k]))
-        return diff
+        return sample1.diff_sample(sample2)
 
     def snp_distance(self, sample1_name, sample2_name):
-        return len(self.diff_samples(sample1_name, sample2_name))
+        return len(self.diff_sample(sample1_name, sample2_name))
 
 def print_matrix(reference_fasta, fastas):
     with open(reference_fasta) as f:
-        reference = Sample().from_fasta(f.read())
+        reference = IntSetSample().from_fasta(f.read())
 
     c = CatWalk('test', reference)
 
-    for file in glob.glob(fastas):
+    for file in glob.glob(fastas)[:10]:
         with open(file) as f:
-            sample = Sample().from_fasta(f.read())
+            sample = IntSetSample().from_fasta(f.read())
         c.add_sample(sample)
-        print(f"loaded {sample.name}")
+        print(f"...")
 
     matrix = list()
     for kA in c.samples:
