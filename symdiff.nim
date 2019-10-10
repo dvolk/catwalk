b49f6f2b907e24ae38413dc8fcfe18561d472bec
Denis Volk <denis.volk@gmail.com>
2019-10-10 19:36:00 +0100

    even more early exit

--- symdiff.nim
+++ symdiff.nim
@@ -38,7 +38,7 @@ proc count_sym_diff*(xs: seq[int], ys: seq[int]) : int =
 
   return count + (last2 - first2)
 
-proc sym_diff1*(xs: seq[int], ys: seq[int], buf: var IntSet) =
+proc sym_diff1*(xs: seq[int], ys: seq[int], buf: var IntSet, max_distance: int) =
   let
     last1 = len(xs) - 1
     last2 = len(ys) - 1
@@ -49,51 +49,67 @@ proc sym_diff1*(xs: seq[int], ys: seq[int], buf: var IntSet) =
   if last1 < 0:
     for y in ys:
       buf.incl(y)
+      if buf.len > max_distance:
+        return
     return
   if last2 < 0:
     for x in xs:
       buf.incl(x)
+      if buf.len > max_distance:
+        return
     return
 
   if xs[last1] < ys[first2]:
     for x in xs:
       buf.incl(x)
+      if buf.len > max_distance:
+        return
     for y in ys:
       buf.incl(y)
+      if buf.len > max_distance:
+        return
     return
 
   while first1 != last1:
     if first2 == last2:
       for j in first1..last1-1:
         buf.incl(xs[j])
+        if buf.len > max_distance:
+          return
       return
     if xs[first1] < ys[first2]:
       buf.incl(xs[first1])
+      if buf.len > max_distance:
+        return
       inc first1
     else:
       if ys[first2] < xs[first1]:
         buf.incl(ys[first2])
+        if buf.len > max_distance:
+          return
       else:
         inc first1
       inc first2
 
   for j in first2..last2-1:
+    if buf.len > max_distance:
+      return
     buf.incl(ys[j])
 
 proc sum_sym_diff1*(xs0, xs1, xs2, xs3, xs4, xs5, xs6, xs7, xs8, xs9, xs10, xs11: seq[int], max_dist: int) : int =
   var
     buf2 = initIntSet()
-  symdiff1(xs0, xs1, buf2)
+  symdiff1(xs0, xs1, buf2, max_dist)
   if buf2.len > max_dist: return max_dist + 1
-  symdiff1(xs2, xs3, buf2)
+  symdiff1(xs2, xs3, buf2, max_dist)
   if buf2.len > max_dist: return max_dist + 1
-  symdiff1(xs4, xs5, buf2)
+  symdiff1(xs4, xs5, buf2, max_dist)
   if buf2.len > max_dist: return max_dist + 1
-  symdiff1(xs6, xs7, buf2)
+  symdiff1(xs6, xs7, buf2, max_dist)
   if buf2.len > max_dist: return max_dist + 1
-  symdiff1(xs8, xs9, buf2)
+  symdiff1(xs8, xs9, buf2, max_dist)
   if buf2.len > max_dist: return max_dist + 1
-  symdiff1(xs10, xs11, buf2)
+  symdiff1(xs10, xs11, buf2, max_dist)
   if buf2.len > max_dist: return max_dist + 1
   result = buf2.len
 
