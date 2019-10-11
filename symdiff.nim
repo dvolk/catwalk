a0a395cfd44da79805d26d84d1ef23689ee5d869
Denis Volk <denis.volk@gmail.com>
2019-10-11 20:58:10 +0100

    reorganise

--- symdiff.nim
+++ symdiff.nim
@@ -5,39 +5,6 @@ import intsets
 import times
 import sequtils
 
-proc count_sym_diff*(xs: seq[int], ys: seq[int]) : int =
-  let
-    last1 = len(xs) - 1
-    last2 = len(ys) - 1
-  var
-    first1 = 0
-    first2 = 0
-    count = 0
-
-  if last1 < 0:
-    return last2 + 1
-  if last2 < 0:
-    return last1 + 1
-
-  if xs[last1] < ys[first2]:
-    return 2 + last1 + last2
-
-  while first1 != last1:
-    if first2 == last2:
-      return count + (last1 - first1)
-
-    if xs[first1] < ys[first2]:
-      inc first1
-      inc count
-    else:
-      if ys[first2] < xs[first1]:
-        inc count
-      else:
-        inc first1
-      inc first2
-
-  return count + (last2 - first2)
-
 proc sym_diff1*(xs: seq[int], ys: seq[int], buf: var IntSet, s1_n_positions: IntSet, s2_n_positions: IntSet, max_distance: int) =
   let
     last1 = len(xs) - 1
@@ -116,105 +83,23 @@ proc sum_sym_diff1*(xs0, xs1, xs2, xs3, xs4, xs5, xs6, xs7: seq[int], s1_n_posit
   symdiff1(xs6, xs7, buf2, s1_n_positions, s2_n_positions, max_dist)
   result = buf2.len
 
-proc sym_diff2*(xs: seq[int], ys: seq[int], buf: var seq[int]) =
-  let
-    last1 = len(xs) - 1
-    last2 = len(ys) - 1
+when isMainModule:
   var
-    first1 = 0
-    first2 = 0
+    xs1, xs2, xs3, xs4, xs5, xs6, xs7, xs8: seq[int]
+    l: int
+    is1 = initIntSet()
+    is2 = initIntSet()
 
-  if last1 < 0:
-    for y in ys:
-      buf.add(y)
-    return
-  if last2 < 0:
-    for x in xs:
-      buf.add(x)
-    return
+  for x in 0..100_000:
+    xs1.add(x)
+    xs2.add(x + 10)
+    xs3.add(x + 100000)
+    xs4.add(x - 100000)
+    xs5.add(x)
+    xs6.add(x * 2)
+    xs7.add(x * 3)
+    xs8.add(x * 4)
 
-  if xs[last1] < ys[first2]:
-    for x in xs:
-      buf.add(x)
-    for y in ys:
-      buf.add(y)
-    return
-
-  while first1 != last1:
-    if first2 == last2:
-      for j in first1..last1-1:
-        buf.add(xs[j])
-      return
-    if xs[first1] < ys[first2]:
-      buf.add(xs[first1])
-      inc first1
-    else:
-      if ys[first2] < xs[first1]:
-        buf.add(ys[first2])
-      else:
-        inc first1
-      inc first2
-
-  for j in first2..last2-1:
-    buf.add(ys[j])
-    
-proc sum_sym_diff2*(xs0, xs1, xs2, xs3, xs4, xs5, xs6, xs7, xs8, xs9, xs10, xs11: seq[int]) : int =
-  var
-    buf2 = newSeqOfCap[int](8000000)
-  symdiff2(xs0, xs1, buf2)
-  symdiff2(xs2, xs3, buf2)
-  symdiff2(xs4, xs5, buf2)
-  symdiff2(xs6, xs7, buf2)
-  symdiff2(xs8, xs9, buf2)
-  symdiff2(xs10, xs11, buf2)
-  result = buf2.deduplicate.len
-  
-#when isMainModule:
-#  var
-#    xs1: seq[int]
-#    xs2: seq[int]
-#    xs3: seq[int]
-#    xs4: seq[int]
-#    xs5: seq[int]
-#    xs6: seq[int]
-#    xs7: seq[int]
-#    xs8: seq[int]
-#    xs9: seq[int]
-#    xs10: seq[int]
-#    xs11: seq[int]
-#    xs12: seq[int]
-#
-#  for x in 0..100_000:
-#    xs1.add(x)
-#    xs2.add(x + 10)
-#    xs3.add(x + 100000)
-#    xs4.add(x - 100000)
-#    xs5.add(x)
-#    xs6.add(x * 2)
-#    xs7.add(x * 3)
-#    xs8.add(x * 4)
-#    xs9.add(x)
-#    xs10.add(x + 1000)
-#    xs11.add(x - 1000)
-#    xs12.add(x)
-#
-#  var time1: float
-#  var l: int
-#
-#  echo "with intset"
-#  time1 = cpuTime()
-#  for _ in 0..1:
-#    l = sum_sym_diff1(xs1, xs2, xs3, xs4, xs5, xs6, xs7, xs8, xs9, xs10, xs11, xs12)
-#  echo cpuTime() - time1
-#  echo l
-#  assert l == 342168
-#
-#  echo "with seq[int]"
-#  time1 = cpuTime()
-#  for _ in 0..1:
-#    l = sum_sym_diff2(xs1, xs2, xs3, xs4, xs5, xs6, xs7, xs8, xs9, xs10, xs11, xs12)
-#  echo cpuTime() - time1
-#  echo l
-#  assert l == 342168
-#  
-#
+  for _ in 0..10:
+    l = sum_sym_diff1(xs1, xs2, xs3, xs4, xs5, xs6, xs7, xs8, is1, is2, 20)
+  assert l == 21
