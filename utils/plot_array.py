f0894a6ce8a7db7e8b7cb7a67bf47a0d5c5f99a3
Denis Volk <denis.volk@gmail.com>
2019-10-15 20:07:06 +0100

    use np.linalg instead of sklean

--- utils/plot_array.py
+++ utils/plot_array.py
@@ -1,7 +1,6 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-from sklearn.linear_model import LinearRegression
 import numpy as np
 import argh
 
@@ -10,15 +9,16 @@ def plot_array(array_filename, xlabel, ylabel, ylim_low, ylim_high, dpi=200, reg
 
     file_content = open(array_filename).read().replace('\n', '')
     Y = eval(file_content)
+    X = np.arange(len(Y))
     
     mpl.rcParams['figure.dpi'] = dpi
 
     plt.plot(Y, 'x')
 
     if regression:
-        X = np.arange(0, len(Y)).reshape(-1, 1)
-        lr = LinearRegression().fit(X, Y)
-        plt.plot(X, lr.predict(X))
+        A = np.vstack([X, np.ones(len(X))]).T
+        m, c = np.linalg.lstsq(A, Y, rcond=None)[0]
+        plt.plot(X, m * X + c, lw=3)
     
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
