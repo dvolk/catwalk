151e081c3539bb1848fd04abb1816e3c9fbe4205
Denis Volk <denis.volk@gmail.com>
2019-11-01 11:46:26 +0000

    update wip database code

--- src/cw_db.nim
+++ src/cw_db.nim
@@ -1,58 +1,40 @@
 import db_sqlite
 import options
-
-type
-  QueueElem* = tuple
-    name: string
-    filepath: string
-    sequence: string
-    status: string
-
-# table config(key, value)
-# table queue (name, filepath, sequence, status)
-# table samples (name, status, diffsets, n_positions, ref_distance)
-# table neighbours (sample_name, neighbours_json)
+import intsets
+import marshal
+import sequtils
 
 let db* = open("cw.sql", "", "", "")
 
 proc create_db*() =
-  db.exec(sql"""create table if not exists config (key, value)""")
-  db.exec(sql"""create table if not exists queue (name, filepath, sequence, status)""")
-  db.exec(sql"""create table if not exists samples (name, status, diffsets, n_positions)""")
-  db.exec(sql"""create table if not exists neighbours (sample_name, neighbours)""")
+  cw_db.db.exec(sql"create table if not exists active_samples (status, diffsets, n_positions)")
+  cw_db.db.exec(sql"create table if not exists all_sample_names (name, id)")
+  cw_db.db.exec(sql"create table if not exists neighbours (id, neighbours)")
+  cw_db.db.exec(sql"create table if not exists queue (filepath)")
 
-proc set_config_key*(key, val: string) =
-  db.exec(sql"""insert or replace into config (key, value) values (?, ?)""", key, val)
+#
+# queue
+#
+proc add_to_queue*(filepath: string) =
+  db.exec(sql"insert into queue (filepath, status) values (?)")
 
-proc add_to_queue*(name, filepath, sequence: string) =
-  db.exec(sql"""insert into queue (name, filepath, sequence, status) values (?, ?, ?, "queued")""")
+proc remove_from_queue*(filepath: string) =
+  db.exec(sql"delete from queue where filepath = ?", filepath)
 
-proc remove_from_queue*(name: string) =
-  db.exec(sql"""delete from queue where name = ?""", name)
+iterator get_queue*(): string =
+  for x in db.fastRows(sql"select filepath from queue"):
+    yield x[0]
 
-proc get_queue*(): seq[QueueElem] =
-  result = @[]
-  for x in db.fastRows(sql"select name, filepath, sequence, status from queue"):
-    var q: QueueElem
-    q.name = x[0]
-    q.filepath = x[1]
-    q.sequence = x[2]
-    q.status = x[3]
-    result.add(q)
+#
+# saving
+#
+proc persist_sample*(index, name, status, diffsets: string, n_positions: IntSet) =
+  var
+    ns = newSeqOfCap[int](n_positions.len)
+  for n in n_positions.items():
+    ns.add(n)
+  db.exec(sql"""insert into active_samples (status, diffsets, n_positions) values (?, ?, ?)""", status, diffsets, $$ns)
+  db.exec(sql"""insert into all_sample_names (name, id) values (?, ?)""", name, index)
 
-proc update_neighbours*(sample_name, neighbours: string) =
-  db.exec(sql"""insert or replace into neighbours values (?, ?)""", sample_name, neighbours)
-
-proc get_sample*(name: string) =
-  db.exec(sql"""select name, status, diffsets, n_positions from samples where name = ?""", name)
-
-iterator all_samples*(): InstantRow =
-  for x in cw_db.db.instantRows(sql"select name, status, diffsets, n_positions from samples"):
-    yield x
-
-iterator all_neighbours*(): InstantRow =
-  for x in cw_db.db.instantRows(sql"select name, status, diffsets, n_positions from samples"):
-    yield x
-
-proc add_sample*(name, status, diffsets, n_positions: string) =
-  db.exec(sql"""insert into samples (name, status, diffsets, n_positions) values (?, ?, ?, ?)""", name, status, diffsets, n_positions)
+proc update_neighbours*(sample_index, neighbours: string) =
+  db.exec(sql"""insert or replace into neighbours values (?, ?)""", sample_index, neighbours)
--- src/cw_demon.nim
+++ src/cw_demon.nim
@@ -13,80 +13,68 @@ import fasta
 import catwalk
 import cw_db
 
-proc load_samples(): seq[Sample] =
-  for x in cw_db.all_samples():
+proc load(c: var CatWalk) =
+  # load active samples
+  for x in cw_db.db.instantRows(sql"select status, diffsets, n_positions from active_samples"):
+    let
+      status = to[SampleStatus](x[0])
+      diffsets = to[CompressedSequence](x[1])
+      n_positions = to[seq[int]](x[2])
     var
       s: Sample
-    s.name = x[0]
-    s.status = to[SampleStatus](x[1])
-    s.diffsets = to[CompressedSequence](x[2])
-    s.n_positions = to[IntSet](x[3])
-    result.add(s)
+    s.status = status
+    s.diffsets = diffsets
+    s.n_positions = initIntSet()
+    for n in n_positions:
+      s.n_positions.incl(n)
+    c.active_samples.add(s)
 
-proc load_neighbours(): Table[int, seq[(int, int)]] =
+  # load neighbours
   var i = 0
-  for x in cw_db.all_neighbours():
-    let
-      neighbours = to[seq[(int, int)]](x[1])
-    result[i] = @[]
-    for neighbour in neighbours:
-      result[i].add(neighbour)
+  for x in cw_db.db.instantRows(sql"select id, neighbours from neighbours"):
+    c.neighbours[i] = to[seq[(int, int)]](x[1])
     inc i
 
-proc main(bind_host: string = "0.0.0.0",
-          bind_port: int = 5000,
-          create_database = false,
-          instance_name: string,
-          reference_name: string,
-          reference_filepath: string,
-          mask_name: string,
-          mask_filepath: string) =
-  let
-    (refheader, refseq) = parse_fasta_file(reference_filepath)
-    reference = new_Sample(reference_filepath, refseq)
-    mask = new_Mask(mask_name, readFile(mask_filepath))
-    max_distance = 20
-    max_n_positions = 130000
-  var
-    c = new_CatWalk(instance_name, reference, mask)
+  # load all_sample_names
+  i = 0
+  for x in cw_db.db.instantRows(sql"select name, id from all_sample_names"):
+    let name = x[0]
+    c.all_sample_names[i] = name
+    c.all_sample_indexes[name] = i
+    inc i
 
-  if create_database:
-    create_db()
-
-  c.samples = load_samples()
-  c.neighbours = load_neighbours()
-
-  var
-    queue: seq[QueueElem]
+proc loop(c: var CatWalk) =
   while true:
-    queue = cw_db.get_queue()
-    if queue.len > 0:
-      var
-        samples = newSeqOfCap[Sample](queue.len)
-      for q in queue:
-        # parse the fasta files
-        let
-          sample_name = q.name
-          (header, sequence) = parse_fasta_file(q.filepath)
-        samples.add(new_Sample(sample_name, sequence))
+    for q_filepath in cw_db.get_queue():
+      echo "adding " & q_filepath
+      let
+        (header, sequence) = parse_fasta_file(q_filepath)
 
-      # add samples to catwalk
-      c.add_samples(samples)
+      c.add_sample(q_filepath, sequence, true) # no need to add to tables
+      let
+        new_index = c.active_samples.len - 1
+        new_sample = c.active_samples[new_index]
+      cw_db.persist_sample($new_index, q_filepath,
+                           $$new_sample.status, $$new_sample.diffsets, new_sample.n_positions)
+      if new_index in c.neighbours:
+        cw_db.update_neighbours($new_index, $$c.neighbours[new_index])
+      else:
+        cw_db.update_neighbours($new_index, "[]")
+      cw_db.remove_from_queue(q_filepath)
 
-      for q in queue:
-        # remove from queue
-        cw_db.remove_from_queue(q.name)
-        let
-          new_sample = c.samples[c.sample_indexes[q.name]]
-        # add processed sample data to database
-        cw_db.add_sample(new_sample.name,
-                         $$new_sample.status,
-                         $$new_sample.diffsets,
-                         $$new_sample.n_positions)
-        cw_db.update_neighbours(new_sample.name,
-                                $$c.get_neighbours(new_sample.name))
-        echo "added " & new_sample.name
+      echo "added " & q_filepath
     sleep(2)
+
+proc main(instance_name: string, reference_filepath: string, mask_filepath: string) =
+  let
+    (_, reference_sequence) = parse_fasta_file(reference_filepath)
+    mask = new_Mask(mask_filepath, readFile(mask_filepath))
+  var
+    c = new_CatWalk(instance_name, reference_filepath, reference_sequence, mask)
+
+  cw_db.create_db()
+  c.load()
+  c.loop()
 
 when isMainModule:
   dispatch(main)
