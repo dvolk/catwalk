e79883a7ee63495e1e4be15ff04175727ab7ceac
Denis Volk <denis.volk@gmail.com>
2019-11-20 14:01:03 +0000

    remove argh package from pycatwalk_load.py

--- src/pycatwalk_load.py
+++ src/pycatwalk_load.py
@@ -1,25 +1,32 @@
 import json
 import glob
-import argh
+import argparse
 import pycatwalk
 
-def main(reference_filepath, mask_filepath, fasta_dir):
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('reference_filepath')
+    parser.add_argument('mask_filepath')
+    parser.add_argument('fasta_dir')
+    args = parser.parse_args()
+
     def fasta_seq(filepath):
         with open(filepath) as f:
             return ''.join(f.read().split('\n')[1:])
-    
-    with open(mask_filepath) as mask:
+
+    with open(args.mask_filepath) as mask:
         pycatwalk.init(name="test instance",
                        reference_name="test reference",
-                       reference_sequence=fasta_seq(reference_filepath),
+                       reference_sequence=fasta_seq(args.reference_filepath),
                        mask_name="test mask",
                        mask_str = mask.read())
-    
-    for i, f in enumerate(glob.glob(fasta_dir)):
+
+    glob_dir = args.fasta_dir + "/*"
+    for i, f in enumerate(glob.glob(glob_dir)):
         pycatwalk.add_sample(name=f, sequence=fasta_seq(f))
         print(f"{i}. {f} neighbours: {len(pycatwalk.neighbours(f))}")
 
     print(json.dumps(pycatwalk.status(), indent=4))
 
 if __name__ == "__main__":
-    argh.dispatch_command(main)
+    main()
