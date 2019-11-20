b2b888e7c27903184ecfe3f9f2b828425ff357fb
Denis Volk <denis.volk@gmail.com>
2019-11-20 16:56:13 +0000

    modify pycatwalk
    - add function and argument logging
    - change pycatwalk_load.py to use reference compression

--- src/pycatwalk.nim
+++ src/pycatwalk.nim
@@ -1,3 +1,4 @@
+import times
 import json
 import tables
 import sequtils
@@ -9,17 +10,34 @@ import catwalk
 
 var c: CatWalk
 
+let
+  log_filepath = "/tmp/catwalk-" & $epochTime().int & "_log.txt"
+  log_handle = open(log_filepath, fmWrite)
+
 proc init(name: string,
           reference_name: string,
           reference_sequence: string,
           mask_name: string,
           mask_str: string) {.exportpy.} =
+  write(log_handle, $(%*{ "function": "init",
+                          "time": $epochTime().int,
+                          "name": name,
+                          "reference_name": reference_name,
+                          "mask_name": mask_name,
+                          "mask_str": mask_str}))
+  flushFile(log_handle)
   c = new_CatWalk(name,
                   reference_name,
                   reference_sequence,
                   new_Mask(mask_name, mask_str))
 
 proc add_sample(name: string, sequence: string, keep: bool = true) {.exportpy.} =
+  write(log_handle, $(%*{ "function": "add_sample",
+                              "time": $epochTime().int,
+                              "name": name,
+                              "sequence": sequence,
+                              "keep": keep }))
+  flushFile(log_handle)
   c.add_sample(name, sequence, keep)
 
 # to avoid recreating it on every call of add_sample_from_refcomp
@@ -31,6 +49,12 @@ tbl_buf["T"] = newSeqOfCap[int](1024)
 tbl_buf["N"] = newSeqOfCap[int](1024)
 
 proc add_sample_from_refcomp(name: string, refcomp_json: string, keep: bool = true) {.exportpy.} =
+  write(log_handle, $(%*{ "function": "add_sample_from_refcomp",
+                              "time": $epochTime().int,
+                              "sample_name": name,
+                              "refcomp_json": refcomp_json,
+                              "keep": keep }))
+  flushFile(log_handle)
   let
     j = parseJson(refcomp_json)
   for c in ["A", "C", "G", "T", "N"]:
@@ -40,9 +64,16 @@ proc add_sample_from_refcomp(name: string, refcomp_json: string, keep: bool = tr
   c.add_sample_from_refcomp(name, tbl_buf, keep)
 
 proc neighbours(sample_name: string): seq[(string, int)] {.exportpy.} =
+  write(log_handle, $(%*{ "function": "neighbours",
+                          "time": $epochTime().int,
+                          "sample_name": sample_name }))
+  flushFile(log_handle)
   return c.get_neighbours(sample_name)
 
 proc status(): Table[string, string] {.exportpy.} =
+  write(log_handle, $(%*{ "function": "status",
+                          "time": $epochTime().int }))
+  flushFile(log_handle)
   result["name"] = c.name
   result["reference_name"] = c.reference_name
   result["reference_sequence_length"] = $c.reference_sequence.len
--- src/pycatwalk_load.py
+++ src/pycatwalk_load.py
@@ -3,6 +3,35 @@ import glob
 import argparse
 import pycatwalk
 
+def fasta_seq(filepath):
+    with open(filepath) as f:
+        return ''.join(f.read().split('\n')[1:])
+
+def ref_compress(refseq, mask, sampleseq, max_n_positions):
+    ret = dict()
+    ret['N'] = list()
+    ret['A'] = list()
+    ret['C'] = list()
+    ret['G'] = list()
+    ret['T'] = list()
+
+    if len(refseq) != len(sampleseq):
+        print("following sample length does not match reference length")
+        return None
+
+    for i, _ in enumerate(refseq):
+        if sampleseq[i] != refseq[i] and not i in mask:
+            if sampleseq[i] not in ["A", "C", "G", "T"]:
+                ret["N"].append(i)
+            else:
+                ret[sampleseq[i]].append(i)
+
+    if len(ret["N"]) > max_n_positions:
+        print(f"following sample has {len(ret['N'])} n positions, which is too many")
+        return None
+
+    return ret
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('reference_filepath')
@@ -10,21 +39,27 @@ def main():
     parser.add_argument('fasta_dir')
     args = parser.parse_args()
 
-    def fasta_seq(filepath):
-        with open(filepath) as f:
-            return ''.join(f.read().split('\n')[1:])
+    refseq = fasta_seq(args.reference_filepath)
 
     with open(args.mask_filepath) as mask:
-        pycatwalk.init(name="test instance",
-                       reference_name="test reference",
-                       reference_sequence=fasta_seq(args.reference_filepath),
-                       mask_name="test mask",
-                       mask_str = mask.read())
+        mask_str = mask.read()
+
+    mask = set([int(x) for x in mask_str.split('\n') if x])
+
+    pycatwalk.init(name="test instance",
+                   reference_name="test reference",
+                   reference_sequence=refseq,
+                   mask_name="test mask",
+                   mask_str=mask_str)
 
     glob_dir = args.fasta_dir + "/*"
     for i, f in enumerate(glob.glob(glob_dir)):
-        pycatwalk.add_sample(name=f, sequence=fasta_seq(f))
-        print(f"{i}. {f} neighbours: {len(pycatwalk.neighbours(f))}")
+        refcomp = ref_compress(refseq, mask, fasta_seq(f), 130000)
+        if refcomp is not None:
+            pycatwalk.add_sample_from_refcomp(name=f, refcomp_json=json.dumps(refcomp))
+            print(f"{i}. {f} neighbours: {len(pycatwalk.neighbours(f))}")
+        else:
+            print(f"skipped {f}")
 
     print(json.dumps(pycatwalk.status(), indent=4))
 
